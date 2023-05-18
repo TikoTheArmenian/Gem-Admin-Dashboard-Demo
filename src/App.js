c16506dfd55393e523d1890a28599d9931ed6d5a
TikoTheArmenian <bdoyant23@columbusacademy.org>
2023-05-18 16:51:04 -0400

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.length} />
+        <main className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Counters
+                  counters={this.state.counters}
+                  onReset={this.handleReset}
+                  onIncriment={this.handleIncriment}
+                  onDelete={this.handleDelete}
+                />
+              }
+            />
+            <Route path="/cum" element={<h1>CUM</h1>} />
+            <Route path="/dick" element={<h1>DICK</h1>} />
+          </Routes>
+        </main>
+      </React.Fragment>
+    );
 }
 
 export default App;
